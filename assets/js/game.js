e737f5aeb4f6ee6462ce0ff1fb7db0763ce9d210
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2022-03-16 17:04:00 -0500

    create player/enemy stats

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+let playerName = window.prompt("What is your robot's name?");
+let playerHealth = 100;
+let playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerHealth, playerAttack);
+
+// enemy robot stats
+let enemyName = 'Herman Roboto';
+let enemyHealth = 50;
+let enemyAttack = 12;
+
+// this creates the fight function
+function fight() {
+	window.alert('Welcome to Robo-Glads!');
+}
+fight();
